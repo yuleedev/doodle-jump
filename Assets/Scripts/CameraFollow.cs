6d1bb226dbe8b3c7ea20243f9f3aa7481d375fa5
yulee <66397574+yuleedev@users.noreply.github.com>
2026-07-14 12:49:23 -0700

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,16 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = .3f;
+
+    private Vector3 currentVelocity;
     
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (target.position.y > transform.position.y)
         {
             Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);
         }
     }
 } 
