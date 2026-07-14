f12a8ba8709742ef755dbbc4d8d4d9b7ccc2a23b
yulee <66397574+yuleedev@users.noreply.github.com>
2026-07-14 11:38:03 -0700

    Player movement, colliders with platformers working

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f; 
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 velocity = rb.linearVelocity;
+                velocity.y = jumpForce;
+                rb.linearVelocity = velocity; 
+            }  
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+
+    private float movement;
+    
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = movement;
+        rb.linearVelocity = velocity;
     }
 }
