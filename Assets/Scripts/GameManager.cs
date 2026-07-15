9f81a0b4d5d35997f1d674a2692cb58bd6fe6145
yulee <66397574+yuleedev@users.noreply.github.com>
2026-07-15 07:33:54 -0700

    Game over screen implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
+    public Camera cam;
+    public GameObject gameOverUI;
+    public float deathMargin = 1f;
+
+    private bool isGameOver = false;
+
     void Start()
     {
-        
+        if (cam == null) cam = Camera.main;
+        if (gameOverUI != null) gameOverUI.SetActive(false);
+        Time.timeScale = 1f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) Restart();
+            return;
+        }
+
+        float bottomOfScreen = cam.transform.position.y - cam.orthographicSize;
+
+        if (player.position.y < bottomOfScreen - deathMargin)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+        if (gameOverUI != null) gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
